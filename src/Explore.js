dcdef0e292075fc0bf6cfc6a057c4314507ad824
noiselike <120612351+noiselike@users.noreply.github.com>
2023-06-04 16:32:52 -0700

    basic ipfs play

--- src/Explore.js
+++ src/Explore.js
@@ -1,10 +1,22 @@
 import React from "react";
+import ReactAudioPlayer from 'react-audio-player';
 
 export function Community() {
   return (
-    <h2>
-      popular <strong>7 days</strong> <br />_______________________
-    </h2>
+    <div>
+      <h2>
+        popular <strong>7 days</strong> <br />_______________________
+      </h2>
+
+      <div className="centered-container">
+        <ReactAudioPlayer
+          src="https://ipfs.io/ipfs/QmcYJWtmvX8bxapTMkHBpnxRKDuqNUvsz5R7zuECHUd2yz?filename=ipfs_test_song.mp3"
+          autoPlay
+          controls
+        />
+      </div>
+    </div>
+
   );
 }
 
