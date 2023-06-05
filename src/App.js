062a30e9d890d602987a72824a4168215f05edcb
noiselike <120612351+noiselike@users.noreply.github.com>
2023-06-04 17:27:37 -0700

    audiotweak

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ReactAudioPlayer from 'react-audio-player';
 
 import Header from './Header.js';
 import Sync from './Sync.js';
@@ -15,19 +16,30 @@ import About from './About.js';
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <Sync />
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Explore />} />
-        <Route path="/community" element={<Community />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/mint" element={<Mint />} />
-        <Route path="/venue" element={<Venue />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </Router>
+    <div>
+      <Router>
+        <Header />
+        <Sync />
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Explore />} />
+          <Route path="/community" element={<Community />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/mint" element={<Mint />} />
+          <Route path="/venue" element={<Venue />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
+
+      <div className="centered-container">
+        <ReactAudioPlayer
+          src="https://ipfs.io/ipfs/QmcYJWtmvX8bxapTMkHBpnxRKDuqNUvsz5R7zuECHUd2yz?filename=ipfs_test_song.mp3"
+          autoPlay
+          controls
+        />
+      </div>
+    </div>
+
   );
 }
 
--- src/Explore.js
+++ src/Explore.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactAudioPlayer from 'react-audio-player';
 
 export function Community() {
   return (
@@ -7,14 +6,6 @@ export function Community() {
       <h2>
         popular <strong>7 days</strong> <br />_______________________
       </h2>
-
-      <div className="centered-container">
-        <ReactAudioPlayer
-          src="https://ipfs.io/ipfs/QmcYJWtmvX8bxapTMkHBpnxRKDuqNUvsz5R7zuECHUd2yz?filename=ipfs_test_song.mp3"
-          autoPlay
-          controls
-        />
-      </div>
     </div>
 
   );
