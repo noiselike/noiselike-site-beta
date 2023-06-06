0303522552c05d66b61df7e4d29d99073a9a315a
Daniel Sukoneck <daniel@sukoneck.com>
2023-06-05 21:45:41 -0700

    testing webamp

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { WalletProvider } from './WalletContext.js';
+import { WalletProvider } from './WalletContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
